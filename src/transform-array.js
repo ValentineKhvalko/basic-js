78798c61858450373d04942aa139140d21e5802c
Valentine <esfeeder@gmail.com>
2020-02-21 18:14:34 +0300

    feat: solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Object.prototype.toString.call(arr) != '[object Array]') throw new Error;
+    if(arr.length == 0) return arr; 
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == '--discard-prev') {
+            if(i > 0) {
+                result.pop();
+            }     
+        } else if (arr[i] == '--double-prev') {
+            if (i > 0){
+                result.push(arr[i - 1]);
+            }
+        } else if (arr[i] == '--double-next') {
+            if (arr.length - 1 > i) {
+                result.push(arr[i + 1]);
+            }
+        } else if (arr[i] == '--discard-next') {
+            if(arr.length - 1 > i) {
+                i++;    
+            }
+        } else {
+            result.push(arr[i]);
+        }  
+    }
+    return result;
 };
