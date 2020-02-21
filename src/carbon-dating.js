4ad34fb7fac6c56aadc037d3b9d35a8bafc2372e
Valentine <esfeeder@gmail.com>
2020-02-21 20:00:11 +0300

    feat: solution what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,6 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   if(typeof sampleActivity !== 'string') return false;
   const sampleActivityNum = parseFloat(sampleActivity);
-  if(isNaN(sampleActivityNum || sampleActivityNum <= 0 || sampleActivityNum > MODERN_ACTIVITY)) return false;
+  if(sampleActivityNum <= 0 || sampleActivityNum > MODERN_ACTIVITY || isNaN(sampleActivityNum)) return false;
   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNum) / (0.693 / HALF_LIFE_PERIOD));
 };
