6d0e699f9da776fc354b613550eb25411e91a89a
Valentine <esfeeder@gmail.com>
2020-02-21 13:27:43 +0300

    feat: solution count-cats, extended-repeater and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(arr) {
+  //throw 'Not implemented';
   // remove line with error and write your code here
+  let count = 0;
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[i].length; j++){
+      if (arr[i][j] === '^^') {
+        count++;
+      }
+    }
+  }
+  return count;
 };
